630f93174751c1905243cf4fa55c2ec1f4eee5aa
zn.ye <znye1996@gmail.com>
2023-12-08 07:09:40 +0000

    feat(RingCore): now support bulk enqueue and bulk dequeue.

--- lockless_ring.h
+++ lockless_ring.h
@@ -3,57 +3,42 @@
 #include <atomic>
 #include <cstdint>
 #include <iostream>
+#include <cmath>
 
 template <typename T>
 struct LLR {
-    std::atomic_uint16_t llr_prod_head_;
-    std::atomic_uint16_t llr_prod_tail_;
+    std::atomic_uint32_t llr_prod_head_;
+    std::atomic_uint32_t llr_prod_tail_;
     int                  llr_size_;
     int                  llr_mask_;
+    uint64_t             llr_drops_;
+    uint64_t             llr_prod_bufs_;
+    uint64_t             llr_prod_bytes_;
+    std::atomic_uint32_t llr_cons_head_;
+    std::atomic_uint32_t llr_cons_tail_;
 
-    uint64_t llr_drops_;
-    uint64_t llr_prod_bufs_;
-    uint64_t llr_prod_bytes_;
-    uint64_t _pad0[11];
-
-    std::atomic_uint16_t llr_cons_head_;
-    std::atomic_uint16_t llr_cons_tail_;
-    uint32_t             prod_offset_;
-    uint32_t             cons_offset_;
-
-    uint64_t _pad1[15];
-#ifdef DEBUG_BUFRING
-    struct mtx* br_lock;
-#endif
     T* llr_ring[0];
 };
 
 template <typename T>
-static int ring_enqueue(struct LLR<T>* llr, T* buf) {
+static int ring_enqueue(struct LLR<T>* llr, T* buf, int bulk_size = 1) {
     bool     success = false;
-    uint16_t local_prod_head, local_cons_tail, local_prod_next;
-    // uint32_t local_prod_offset;
+    uint32_t local_prod_head, local_cons_tail, local_prod_next;
     do {
-        local_prod_head = llr->llr_prod_head_;  // 0 ~ UINT_MAX
-        local_cons_tail = llr->llr_cons_tail_;  // 0 ~ UINT_MAX
-        local_prod_next = local_prod_head + 1;
-        // if (local_prod_next < local_prod_head) {  // flipping
-        //     // need to update prod_offset.
-        //     local_prod_offset =
-        //         (llr->prod_offset_ + (uint32_t)(local_prod_head) + 1) %
-        //         llr->llr_size_;  // for example: 65535 + 1 % 200 = 136, llr_ring[136] is the new start of ring.
-        //     std::cout << "local prod head: " << local_prod_head << " local prod next: " << local_prod_next
-        //               << " prod offset: " << llr->prod_offset_ << " local prod offset: " << local_prod_offset
-        //               << " local cons tail: " << local_cons_tail << std::endl;
-        // }
-        if ((local_prod_next) % llr->llr_size_ == local_cons_tail % llr->llr_size_) {
-            return -1;  // no enough bufs
+        local_prod_head = llr->llr_prod_head_;
+        local_cons_tail = llr->llr_cons_tail_;
+        local_prod_next = local_prod_head + bulk_size;
+        // check if there are enough bufs. free entries = mask + cons tail - prod head
+        if (llr->llr_mask_ + local_cons_tail - local_prod_head < bulk_size) {
+            return -1;
         }
         success = llr->llr_prod_head_.compare_exchange_strong(
             local_prod_head, local_prod_next);  // could not use table_prod_next(0 ~ size - 1)
     } while (!success);
 
-    llr->llr_ring[((uint32_t)local_prod_head) % llr->llr_size_] = buf;
+    for (int i = 0; i < bulk_size; ++i) {
+        llr->llr_ring[(local_prod_head + i) % llr->llr_size_] = buf + i;
+    }
 
     // if there are other enqueues in progress that preceeded us, we need to wait for them to complete.
     while (llr->llr_prod_tail_ != local_prod_head)
@@ -66,30 +51,24 @@ static int ring_enqueue(struct LLR<T>* llr, T* buf) {
 }
 
 template <typename T>
-static T* ring_dequeue(struct LLR<T>* llr) {
-    uint16_t local_cons_head, local_cons_next, local_prod_tail;
+static T* ring_dequeue(struct LLR<T>* llr, int bulk_size = 1) {
+    uint32_t local_cons_head, local_cons_next, local_prod_tail;
     T*       buf;
     bool     success = false;
 
     do {
-        std::atomic_thread_fence(std::memory_order_acquire);
-        // local_cons_head = std::atomic_load(&llr->llr_cons_head_, std::memory_order_acquire);
         local_cons_head = llr->llr_cons_head_;
         local_prod_tail = llr->llr_prod_tail_;
-        local_cons_next = local_cons_head + 1;
-        // local_cons_next may be zero.
-        if(local_cons_head > local_cons_next) { // flipping
-            // 
-        }
-        if (local_prod_tail % llr->llr_size_ == local_cons_head % llr->llr_size_) {
-            // std::cout << "no enough bufs" << std::endl;
+        local_cons_next = local_cons_head + bulk_size;
+        // check if there are enough data, used entries = prod tail - cons head
+        if (local_prod_tail - local_cons_head < bulk_size) {
+            std::cout << "no enough data" << std::endl;
             return nullptr;  // no enough bufs
         }
         success = llr->llr_cons_head_.compare_exchange_strong(local_cons_head, local_cons_next);
     } while (!success);
 
     buf = llr->llr_ring[local_cons_head % llr->llr_size_];
-    // llr->br_ring[cons_head] = nullptr;
 
     // if there are other dequeues in progress that preceeded us, we need to wait for them to complete.
     while (llr->llr_cons_tail_ != local_cons_head)
@@ -100,16 +79,12 @@ static T* ring_dequeue(struct LLR<T>* llr) {
 }
 
 template <typename T>
-static struct LLR<T>* InitStructLLR(int size) {
-    struct LLR<T>* llr = (struct LLR<T>*)calloc(sizeof(struct LLR<T>) + sizeof(T*) * size, 1);
-    llr->llr_size_     = size;
-    llr->llr_mask_     = size - 1;
+static struct LLR<T>* InitStructLLR(uint8_t power) {
+    size_t         size = pow(2, power);
+    struct LLR<T>* llr  = (struct LLR<T>*)calloc(sizeof(struct LLR<T>) + sizeof(T*) * size, 1);
+    llr->llr_size_      = size;
+    llr->llr_mask_      = size - 1;
     return llr;
-}
-
-template <typename T>
-static __inline int ring_empty(struct LLR<T> * llr) {
-    return (llr->llr_cons_head_ == llr->llr_prod_tail_);
 }
 
 template <typename T>
@@ -117,7 +92,8 @@ static __inline uint16_t ring_count(struct LLR<T>* llr) {
     return llr->llr_prod_tail_ - llr->llr_cons_head_;
 }
 
-// template <typename T>
-// static __inline int ring_full(struct LLR<T>* llr) {
-//     return (llr->llr_prod_head_ + 1) & llr->llr_mask_ == llr->llr_cons_tail_;
-// }
+template <typename T>
+static __inline uint16_t ring_free(struct LLR<T>* llr) {
+    return llr->llr_mask_ + llr->llr_cons_tail_ - llr->llr_prod_head_;
+}
+
--- main.cc
+++ main.cc
@@ -2,6 +2,8 @@
 #include <string>
 #include <thread>
 #include <string.h>
+#include <csignal>
+#include <unistd.h>
 
 struct Person {
     int    age;
@@ -18,40 +20,36 @@ static uint64_t prodpkt2 = 0;
 static uint64_t conspkt1 = 0;
 static uint64_t conspkt2 = 0;
 
-void ProducerThreadFunc(struct LLR<Person>* llr, uint64_t* prodpkt, Person* p, int size) {
-    int i = 0;
+void ProducerThreadFunc(struct LLR<Person>* llr, uint64_t* prodpkt, Person* p, int ring_size) {
+    int i            = 0;
+    int enqueue_size = 7;
     while (prod_flag) {
-        // std::this_thread::sleep_for(std::chrono::microseconds(1));
-        int ret = ring_enqueue(llr, &p[i % size]);
+        int ret = ring_enqueue(llr, &p[i % ring_size], enqueue_size);
         if (ret == -1) {
-            // cout << "ring full" << std::endl;
+            // "ring full"
             continue;
         }
-        ++(*prodpkt);
+        (*prodpkt) += enqueue_size;
         ++i;
     }
 }
 
 void ConsumerThreadFunc(struct LLR<Person>* llr, uint64_t* conspkt, int thread_id) {
-    Person* p = nullptr;
+    Person* p    = nullptr;
+    int     dequeue_size = 5;
     while (cons_flag) {
-        std::this_thread::sleep_for(std::chrono::microseconds(1));
-        p = ring_dequeue(llr);
+        p = ring_dequeue(llr, dequeue_size);
         if (p == nullptr) {
             continue;
         }
-        // cout << "Thread ID : " << thread_id <<  ", Age:" << p->age << std::endl;
-        ++(*conspkt);
+        (*conspkt) += dequeue_size;
     }
 }
-
-#include <csignal>
-#include <unistd.h>
 
 // Signal Handler for SIGINT
 void signalHandler(int signum) {
     prod_flag = false;
-    // sleep(2);
+    sleep(1);
     cons_flag = false;
 }
 
@@ -59,43 +57,37 @@ void LKTest() {
     // Register signal and signal handler
     signal(SIGINT, signalHandler);
 
-    auto   llr = InitStructLLR<Person>(200);  // size: 100 type: Person
-    Person p[200];
+    auto   llr = InitStructLLR<Person>(10);  // size: 2 ^ 10 = 1024 type: Person
+    Person p[1024];
     memset(p, 0x00, sizeof(p));
-    for (int i = 0; i < 200; ++i) {
+    for (int i = 0; i < 1024; ++i) {
         p[i].age = i * 3;
     }
-    // set cursor flipping..
-    // llr->llr_cons_head_ = mod - 1000;
-    // llr->llr_cons_tail_ = mod - 1000;
-    // llr->llr_prod_head_ = mod - 1000;
-    // llr->llr_prod_tail_ = mod - 1000;
-    std::thread prod1(ProducerThreadFunc, llr, &prodpkt1, p, 200);
-    // std::thread prod2(ProducerThreadFunc, llr, &prodpkt2, p, 200);
+
+    std::thread prod1(ProducerThreadFunc, llr, &prodpkt1, p, 1024);
+    std::thread prod2(ProducerThreadFunc, llr, &prodpkt2, p, 1024);
     std::thread cons1(ConsumerThreadFunc, llr, &conspkt1, 1);
-    // std::thread cons2(ConsumerThreadFunc, llr, &conspkt2, 2);
+    std::thread cons2(ConsumerThreadFunc, llr, &conspkt2, 2);
 
     // sleep(1);
     if (prod1.joinable()) {
         prod1.join();
     }
-    // if (prod2.joinable()) {
-    //     prod2.join();
-    // }
+    if (prod2.joinable()) {
+        prod2.join();
+    }
     if (cons1.joinable()) {
         cons1.join();
     }
-    // if (cons2.joinable()) {
-    //     cons2.join();
-    // }
+    if (cons2.joinable()) {
+        cons2.join();
+    }
 
-    // print cons_head cons_tail prod_head prod_tail of llr
     std::cout << std::endl;
     std::cout << "prod1: " << prodpkt1 << " prod2: " << prodpkt2 << " sum: " << prodpkt1 + prodpkt2 << std::endl;
     std::cout << "cons1: " << conspkt1 << " cons2: " << conspkt2 << " sum: " << conspkt1 + conspkt2 << std::endl;
     std::cout << "cons_head: " << llr->llr_cons_head_ << " cons_tail: " << llr->llr_cons_tail_
               << " prod_head: " << llr->llr_prod_head_ << " prod_tail: " << llr->llr_prod_tail_ << std::endl;
-
     std::cout << "ring count: " << ring_count(llr) << std::endl;
 }
 
